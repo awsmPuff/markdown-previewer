6635309ae1998663d185982bfa6f3f0c6b81cf2f
awsmPuff <itsanthea915@gmail.com>
2022-05-20 05:30:01 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import {marked} from 'marked';
+
+// a heading element (H1 size), 
+// a sub heading element (H2 size), 
+// a link, 
+// inline code, 
+// a code block,
+// a list item, 
+// a blockquote, 
+// an image, and 
+// bolded text.
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const [text, setText] = useState(`# H1
+## H2
+[title](https://www.example.com)
+\`code\`
+\`\`\` 
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+- First item
+- Second item
+- Third item
+> blockquote
+![alt text](image.jpg)
+**bold text**
+    `);
+
+marked.setOptions({
+    breaks:true
+})
+
+    return (
+    
+    <div className='App'>
+        <h1>Preview WHAT U Type</h1>
+        <p>Input:</p>
+        <textarea 
+        id="editor" 
+        onChange={e => setText(e.target.value)} 
+        value={text} 
+        rows="10"
+        />
+        <p>Output:</p>
+        <div 
+        id="preview" 
+        dangerouslySetInnerHTML={{
+            __html: marked(text)
+        }}>
+        </div>
     </div>
-  );
+
+    );
+
 }
 
 export default App;
